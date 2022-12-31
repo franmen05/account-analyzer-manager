e07c08b7f612cff2cfb84618f735891fbeec5880
franmen05 <franmen05@gmail.com>
2022-12-30 20:12:18 -0400

    remove liquibase

--- src/main/webapp/app/entities/entity-routing.module.ts
+++ src/main/webapp/app/entities/entity-routing.module.ts
@@ -4,6 +4,16 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'deduction',
+        data: { pageTitle: 'Deductions' },
+        loadChildren: () => import('./deduction/deduction.module').then(m => m.DeductionModule),
+      },
+      {
+        path: 'aa',
+        data: { pageTitle: 'AAS' },
+        loadChildren: () => import('./aa/aa.module').then(m => m.AAModule),
+      },
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ]),
   ],
